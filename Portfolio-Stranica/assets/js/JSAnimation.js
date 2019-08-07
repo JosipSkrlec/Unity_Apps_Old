f56f9a7edc10b68988bbe011632fe46cd16b20b6
Josip Skrlec <jskrlec1997@gmail.com>
2019-08-07 09:43:13 +0200

    Popravak i rename postojecih projekta, dodavanje projekta portfolio i brisanje nepotrebnog projekta

---
+++ Portfolio-Stranica/assets/js/JSAnimation.js
@@ -0,0 +1,57 @@
+//
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('vrijeme').innerHTML = " ST = " + h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+
+// SLJDECA FUNKCIJA PRIKAZUJE SLOVA PO REDU
+function typeEffect(element, speed) {
+	var text = element.innerHTML;
+	element.innerHTML = "";
+	console.log("Počinjem pisati slova");
+
+	var i = 0;
+	var timer = setInterval(function() { // provjera da li je *, ako je onda se stavlja novi red
+		if(text.charAt(i) == "*"){
+			var br = document.createElement("br");
+			element.appendChild(br);
+
+			i++;
+		}
+    else if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+			console.log("gotov sam sa pisanjem slova");
+    }
+  }, speed);
+}
+
+
+// application
+var speed = 100;
+var h1 = document.querySelector('h4');
+// var p = document.querySelector('p');
+var delay = h1.innerHTML.length * speed + speed;
+
+// type affect to header
+typeEffect(h1, speed);
+
+
+// type affect to body
+// setTimeout(function(){
+//   p.style.display = "inline-block";
+//   typeEffect(p, speed);
+// }, delay);
