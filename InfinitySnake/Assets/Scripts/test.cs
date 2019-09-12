b3c4725eb31af67fb3cd66efc9c756ad8564ca59
Josip Skrlec <jskrlec1997@gmail.com>
2019-09-12 21:20:48 +0200

    Dodavanje responzivnog spawn-a checkpointova

--- InfinitySnake/Assets/Scripts/test.cs
+++ InfinitySnake/Assets/Scripts/test.cs
@@ -7,37 +7,26 @@ public class test : MonoBehaviour
     public GameObject jab;
 
     public GameObject test01;
-    public GameObject test02;
+
+    public GameObject CrvenaVrata;
+    public GameObject LjubicastaVrata;
+    public GameObject ZelenaVrata;
 
 
     // Use this for initialization
     void Start()
     {
-
-
+        Debug.Log("ASPECT " + " " + Camera.main.aspect);
         spawnaj();
+
+        spawnajprolaze();
 
     }
 
 
     private void Update()
     {
-        float height = Camera.main.orthographicSize * 2;
-        float width = height * Screen.width / Screen.height;
 
-        test02.transform.localScale = new Vector3((height / 4f) , 0.25f,0.25f);
-
-        Vector3 pos = new Vector3(0.25f, 0.0f, 0.0f);
-        pos = Camera.main.ViewportToWorldPoint(pos);
-
-        Instantiate(test02, pos, Quaternion.identity);
-
-        //float x = Random.Range(0.05f, 0.95f);
-        //float y = Random.Range(0.05f, 0.95f);
-        //Vector3 pos = new Vector3(x, y, 10.0f);
-        //pos = Camera.main.ViewportToWorldPoint(pos);
-
-        //Instantiate(jab, pos, Quaternion.identity);
 
     }
 
@@ -54,6 +43,33 @@ public class test : MonoBehaviour
             Instantiate(jab, pos, Quaternion.identity);
         }
 
+    }
+
+    void spawnajprolaze()
+    {
+        // varijable za spawn
+        float height = Camera.main.orthographicSize * 2;
+        float width = height * Screen.width / Screen.height;
+
+        // 3.0f je kolicina na koliko djelova zelimo podjeliti screen
+        CrvenaVrata.transform.localScale = new Vector3((width / 3.0f), 0.25f, 0.25f);
+
+        // POZICIJA SPAWNANJA
+        // 0.0f je skroz desno, 0.5f je u sredini i 1.0f je skroz desno
+        Vector3 pos = new Vector3(0.15f, 0.0f, 0.0f);
+        pos = Camera.main.ViewportToWorldPoint(pos);
+
+        Instantiate(CrvenaVrata, pos, Quaternion.identity);
+
+        Vector3 pos1 = new Vector3(0.5f, 0.0f, 0.0f);
+        pos1 = Camera.main.ViewportToWorldPoint(pos1);
+
+        Instantiate(CrvenaVrata, pos1, Quaternion.identity);
+
+        Vector3 pos2 = new Vector3(0.85f, 0.0f, 0.0f);
+        pos2 = Camera.main.ViewportToWorldPoint(pos2);
+
+        Instantiate(CrvenaVrata, pos2, Quaternion.identity);
     }
 
 
