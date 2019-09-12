2590775afbd431346658c4428ed2644bb52ed7d8
Josip Skrlec <jskrlec1997@gmail.com>
2019-09-12 20:42:31 +0200

    Stvaranje nove scene u kojoj ce zmija ici samo naprijed

---
+++ InfinitySnake/Assets/Scripts/ScreenSizeChecker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSizeChecker : MonoBehaviour
+{
+    // 
+    public GameObject HeadSnake;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // uzimanje pozicije tijela i provjeravanje gdje se nalazi na screen-u
+        Vector3 pos = Camera.main.WorldToViewportPoint(HeadSnake.transform.position);
+
+        // TODO - napraviti umiranje za lijevo i desno
+        if (pos.x < 0.0)
+        {
+            Debug.Log("LIJEVO");
+        }
+        if (1.0 < pos.x)
+        {
+            Debug.Log("DESNO");
+        }
+        if (pos.y < 0.0)
+        {
+            Debug.Log("ISPOD");
+        }
+        if (1.0 < pos.y)
+        {
+            Debug.Log("IZNAD");
+        }
+
+    }//update
+
+
+
+
+}
--- InfinitySnake/Assets/Scripts/test.cs
+++ InfinitySnake/Assets/Scripts/test.cs
@@ -7,6 +7,8 @@ public class test : MonoBehaviour
     public GameObject jab;
 
     public GameObject test01;
+    public GameObject test02;
+
 
     // Use this for initialization
     void Start()
@@ -14,6 +16,28 @@ public class test : MonoBehaviour
 
 
         spawnaj();
+
+    }
+
+
+    private void Update()
+    {
+        float height = Camera.main.orthographicSize * 2;
+        float width = height * Screen.width / Screen.height;
+
+        test02.transform.localScale = new Vector3((height / 4f) , 0.25f,0.25f);
+
+        Vector3 pos = new Vector3(0.25f, 0.0f, 0.0f);
+        pos = Camera.main.ViewportToWorldPoint(pos);
+
+        Instantiate(test02, pos, Quaternion.identity);
+
+        //float x = Random.Range(0.05f, 0.95f);
+        //float y = Random.Range(0.05f, 0.95f);
+        //Vector3 pos = new Vector3(x, y, 10.0f);
+        //pos = Camera.main.ViewportToWorldPoint(pos);
+
+        //Instantiate(jab, pos, Quaternion.identity);
 
     }
 
@@ -30,21 +54,61 @@ public class test : MonoBehaviour
             Instantiate(jab, pos, Quaternion.identity);
         }
 
-        Vector3 pos1 = new Vector3(0f, 0.5f, 10.0f);
-        pos1 = Camera.main.ViewportToWorldPoint(pos1);
-        Instantiate(test01, pos1, Quaternion.identity);
-
-        Vector3 pos2 = new Vector3(1f, 0.5f, 10.0f);
-        pos2 = Camera.main.ViewportToWorldPoint(pos2);
-        Instantiate(test01, pos2, Quaternion.identity);
-
     }
 
-
-    private void Update()
-    {
-       
-    }
 
 
 }
+
+
+// PRVA SCENA SAMPLE SCENA
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class test : MonoBehaviour
+//{
+//    public GameObject jab;
+
+//    public GameObject test01;
+
+//    // Use this for initialization
+//    void Start()
+//    {
+
+
+//        spawnaj();
+
+//    }
+
+//    void spawnaj()
+//    {
+//        // spawn random u screen size (RESPONZIVNO JE)
+//        for (int a = 0; a <= 5; a++)
+//        {
+//            float x = Random.Range(0.05f, 0.95f);
+//            float y = Random.Range(0.05f, 0.95f);
+//            Vector3 pos = new Vector3(x, y, 10.0f);
+//            pos = Camera.main.ViewportToWorldPoint(pos);
+
+//            Instantiate(jab, pos, Quaternion.identity);
+//        }
+
+//        Vector3 pos1 = new Vector3(0f, 0.5f, 10.0f);
+//        pos1 = Camera.main.ViewportToWorldPoint(pos1);
+//        Instantiate(test01, pos1, Quaternion.identity);
+
+//        Vector3 pos2 = new Vector3(1f, 0.5f, 10.0f);
+//        pos2 = Camera.main.ViewportToWorldPoint(pos2);
+//        Instantiate(test01, pos2, Quaternion.identity);
+
+//    }
+
+
+//    private void Update()
+//    {
+
+//    }
+
+
+//}
