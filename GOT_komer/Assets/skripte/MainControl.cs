0d13e98d8537f1871f7ee1d53a8f7e43ee3ba35d
Josip Skrlec <jskrlec1997@gmail.com>
2019-09-18 17:44:08 +0200

    Dodavanje panela s kontrolama

---
+++ GOT_komer/Assets/skripte/MainControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainControl : MonoBehaviour {
+
+    #region public Varijable
+    public GameObject PrviPanel;
+    public GameObject DrugiPanel;
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        PrviPanel.SetActive(true);
+        DrugiPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+		
+	}
+
+    public void OtvoriDrugiScreen()
+    {
+        PrviPanel.SetActive(false);
+        DrugiPanel.SetActive(true);
+
+    }
+
+
+
+
+}
