5cbe67c1afdc47e5a4df43d79074b8505c441fa6
Josip Skrlec <jskrlec1997@gmail.com>
2019-09-29 12:14:52 +0200

    Dodavanje zadnje scene, i dodavanje gumbova u scrool view

---
+++ GOT_komer/Assets/skripte/MainControlHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainControlHelper : MonoBehaviour {
+
+    public GameObject ContentParent;
+    public GameObject buttonPrefab;
+
+    int brojac = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void addnewbutton()
+    {
+        brojac++;
+        GameObject newButton = Instantiate(buttonPrefab) as GameObject;
+        newButton.name = "ButtonSelectedSeats" + brojac;
+
+
+
+
+        newButton.transform.SetParent(ContentParent.transform, false);
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
