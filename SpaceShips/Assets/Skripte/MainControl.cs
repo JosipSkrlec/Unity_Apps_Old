c9791672b1135e6a8bcfd69ce990cccf8cdd2231
Josip Skrlec <jskrlec1997@gmail.com>
2019-11-30 18:02:40 +0100

    Game creation, shooting and another features

--- SpaceShips/Assets/Skripte/MainControl.cs
+++ SpaceShips/Assets/Skripte/MainControl.cs
@@ -7,7 +7,6 @@ public class MainControl : MonoBehaviour
     #region Game Cotrol Variables
     public float Cooldown;
 
-
     private protected int NumberOfWaves;
 
     #endregion
@@ -50,9 +49,19 @@ public class MainControl : MonoBehaviour
         {
             Vector3 PositionOfPlayer = this.gameObject.transform.position;
 
-            Debug.Log(PositionOfPlayer + " " + this.gameObject.transform.localPosition);
+            //Instantiate(Projectile01,new Vector3(PositionOfPlayer.x, PositionOfPlayer.y + 0.5f, PositionOfPlayer.z), );
 
-            Instantiate(Projectile01,new Vector3(PositionOfPlayer.x, PositionOfPlayer.y + 0.5f, PositionOfPlayer.z), Quaternion.identity);
+            GameObject GO_ForSpawn = (GameObject)Instantiate(Projectile01);
+            GO_ForSpawn.transform.position = new Vector3(PositionOfPlayer.x, PositionOfPlayer.y + 0.5f, PositionOfPlayer.z);
+            GO_ForSpawn.transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+
+            GameObject GO_ForSpawn1 = (GameObject)Instantiate(Projectile01);
+            GO_ForSpawn1.transform.position = new Vector3(PositionOfPlayer.x, PositionOfPlayer.y + 0.5f, PositionOfPlayer.z);
+            GO_ForSpawn1.transform.eulerAngles = new Vector3(0.0f, 0.0f, 10.0f);
+
+            GameObject GO_ForSpawn2 = (GameObject)Instantiate(Projectile01);
+            GO_ForSpawn2.transform.position = new Vector3(PositionOfPlayer.x, PositionOfPlayer.y + 0.5f, PositionOfPlayer.z);
+            GO_ForSpawn2.transform.eulerAngles = new Vector3(0.0f, 0.0f, -10.0f);
 
             time = 0.0f;
         }
