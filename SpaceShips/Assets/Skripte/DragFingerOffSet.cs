2f51aad57570ad9f0875f853dbe8465f5384b286
Josip Skrlec <jskrlec1997@gmail.com>
2019-11-30 09:56:52 +0100

    SpaceShips init

---
+++ SpaceShips/Assets/Skripte/DragFingerOffSet.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragFingerOffSet : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    private float deltaX, deltaY;
+    private Rigidbody2D rb;
+
+    bool jedanput = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            deltaX = mousePosition.x - transform.position.x;
+            deltaY = mousePosition.y - transform.position.y;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(mousePosition);
+            rb.MovePosition(new Vector2(mousePosition.x - deltaX, mousePosition.y - deltaY));
+
+
+            //mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //direction = (mousePosition - transform.position).normalized;
+            //rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+#endif
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+
+                case TouchPhase.Began:
+                    deltaX = touchPos.x - transform.position.x;
+                    deltaY = touchPos.y - transform.position.y;
+
+                    break;
+
+                case TouchPhase.Moved:
+                    rb.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
+
+                    break;
+
+                case TouchPhase.Ended:
+                    rb.velocity = Vector2.zero;
+
+                    break;
+            }
+
+
+        }    
+
+    }
+
+
+
+}
