1031d97a0951748ffcd799e1f1ee690eb1068124
JosipSkrlec <jskrlec1997@gmail.com>
2021-01-11 16:52:24 +0100

    Dodavanje back to top button-a i update gallery

--- Portfolio-Stranica/GlavnaStranica/JosipSkrlec/assets/js/main.js
+++ Portfolio-Stranica/GlavnaStranica/JosipSkrlec/assets/js/main.js
@@ -4,6 +4,30 @@
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
+// Button za povratak na pocetak stranice
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Button za povratak na pocetak stranice DO TUDA
+
+
 (function($) {
 
 	skel.breakpoints({
