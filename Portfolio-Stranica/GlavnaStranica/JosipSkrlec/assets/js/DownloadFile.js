39189d639ff613d1798a4986b15d4a9dff52cdd9
JosipSkrlec <jskrlec1997@gmail.com>
2021-06-16 16:36:38 +0200

    Update

--- Portfolio-Stranica/GlavnaStranica/JosipSkrlec/assets/js/DownloadFile.js
+++
@@ -1,9 +0,0 @@
-function downloadURI(uri, name) {
-  var link = document.createElement("a");
-  link.download = name;
-  link.href = uri;
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-  delete link;
-}
